a7921c53efffd1193ff5e8a1073cfbfe2017d081
pingvin12 <fenyesj@gmail.com>
2022-04-15 04:28:51 +0200

    final final revision

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import EntityLister as eli
 if __name__ == '__main__':
     filex = open('text.txt', 'r') # Everything is loaded from a text file
     genes = []
+
     for x in filex.readlines():
         s = x.split()
         if len(s) == 0: # loading every sentence to an array for better handling
@@ -12,6 +13,13 @@ if __name__ == '__main__':
             for f in s:
                 genes.append(f)
     genes = np.array(genes, dtype=object)
-    el = eli.EntityLister(3, 3, genes) # creating a lister instance for main logic
-    el.start_evolution(2)
+    print(f'Please type in the input values in the following order:\n initial population amount, max amount of weight, generation limit, offspring multiplier\n don\'t worry error messages may appear, it\'s completely normal.')
+    init_pop = int(input())
+    weight = int(input())
+    generationlimit = int(input())
+    offspringmultiplier = int(input())
+
+    el = eli.EntityLister(init_pop, weight, genes) # creating a lister instance for main logic
+    el.start_evolution(generationlimit, offspringmultiplier)
     el.getEvolutionDF().to_excel('Evolution.xlsx')
+    print('You can now view the simulation in the Evolution.xlsx file.')
